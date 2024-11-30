c18c94568033a38139bd6314f9bafd2f76df76e5
erfandwi <erfan@jogjacamp.com>
2024-11-30 07:23:29 +0700

    update scan

--- scan.go
+++ scan.go
@@ -1,97 +1,121 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "regexp"
+    "strings"
+    "sync"
 )
 
 func main() {
-	// Definisikan flag untuk menerima argumen --target
-	var targetFolder string
-	flag.StringVar(&targetFolder, "target", "", "Path ke folder yang ingin diperiksa")
-	flag.Parse()
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: go run script.go /path/")
+        return
+    }
 
-	if targetFolder == "" {
-		fmt.Println("Harap tentukan path folder dengan menggunakan flag --target")
-		return
-	}
+    root := os.Args[1]
+    patterns := []string{
+                "787backlink.xyz",
+                "binusrun.com",
+                "seokeras.com",
+                "jasaiklan.lol",
+                "umrohkemenag.org",
+                "a.mikomallkopo.com",
+                "purefine.online",
+                "twins-4e372.web.app",
+                "forbesstream.com",
+                "pinjam100.site",
+                "fixtechcs.net",
+                "dw88.cwhonors.org",
+                "w88.sepangracingteam.com",
+                "ant1rungk4d",
+                "ampshopify.store",
+                "petirjago.site",
+                "seoinclude.pages.dev",
+                "includes-page.com",
+                "wp.rpka.org",
+                "admin.rpka.org",
+                "rpka.org",
+                "hedon77official.xyz",
+                "linkgacorthailand.xyz",
+                "dp288.live",
+                "bobabotui.lol",
+                "seobeton.team",
+                "aHR0cHM6Ly95b2licmUuY29tL3NoZWxsL2FkbWluLnR4dA",
+                "judi",
+                "jackpot",
+                "pragmatic",
+                "casino",
+                "winrate",
+                "gacor",
+                "togel",
+                "maxwin",
+                "scatter",
+                "nextcloud",
+                "owncloud",
+        }
 
-	// Kata kunci yang ingin Anda cocokkan
-	keywords := []string{
-		"787backlink.xyz",
-		"binusrun.com",
-		"seokeras.com",
-		"jasaiklan.lol",
-		"umrohkemenag.org",
-		"a.mikomallkopo.com",
-		"purefine.online",
-		"twins-4e372.web.app",
-		"forbesstream.com",
-		"pinjam100.site",
-		"fixtechcs.net",
-		"dw88.cwhonors.org",
-		"w88.sepangracingteam.com",
-		"ant1rungk4d",
-		"ampshopify.store",
-		"petirjago.site",
-		"seoinclude.pages.dev",
-		"includes-page.com",
-		"wp.rpka.org",
-		"admin.rpka.org",
-		"rpka.org",
-		"hedon77official.xyz",
-		"linkgacorthailand.xyz",
-		"dp288.live",
-		"bobabotui.lol",
-		"seobeton.team",
-		"aHR0cHM6Ly95b2licmUuY29tL3NoZWxsL2FkbWluLnR4dA",
-		"judi",
-		"jackpot",
-		"pragmatic",
-		"casino",
-		"winrate",
-		"gacor",
-		"togel",
-		"maxwin",
-		"scatter",
-		"nextcloud",
-		"owncloud",
-	}
+    regex := regexp.MustCompile(`(?i)` + strings.Join(patterns, "|"))
 
-	// Lakukan pemindaian pada semua file di folder
-	err := filepath.Walk(targetFolder, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+    var wg sync.WaitGroup
+    fileChan := make(chan string)
 
-		// Memeriksa apakah file adalah symlink
-		if !info.Mode().IsRegular() {
-			return nil // Mengabaikan symlink
-		}
+    go func() {
+        err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+            if err != nil {
+                return err
+            }
+            if !info.IsDir() {
+                fileChan <- path
+            }
+            return nil
+        })
+        if err != nil {
+            fmt.Println("Error:", err)
+        }
+        close(fileChan)
+    }()
 
-		if !info.IsDir() {
-		// Hanya memeriksa file .php saja
-		//if !info.IsDir() && strings.HasSuffix(info.Name(), ".php") {
-			content, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+    for i := 0; i < 10; i++ { // Number of goroutines
+        wg.Add(1)
+        go func() {
+            defer wg.Done()
+            for path := range fileChan {
+                scanFile(path, regex)
+            }
+        }()
+    }
 
-			// Cocokkan dengan kata kunci
-			for _, keyword := range keywords {
-				if strings.Contains(string(content), keyword) {
-					fmt.Printf("File %s keyword: %s\n", path, keyword)
-				}
-			}
-		}
-		return nil
-	})
+    wg.Wait()
+}
 
-	if err != nil {
-		fmt.Println("Terjadi kesalahan:", err)
-	}
+func scanFile(path string, regex *regexp.Regexp) {
+    file, err := os.Open(path)
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    lineNumber := 1
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            fmt.Println("Error reading file:", err)
+            return
+        }
+        if regex.MatchString(line) {
+            matchedPattern := regex.FindString(line)
+            fmt.Printf("%s keyword %s at line %d\n", path, matchedPattern, lineNumber)
+        }
+        lineNumber++
+    }
 }
